0a5d1ec79cc2094416a51159c02e86802c9c3646
fawzibazari <fawzi.bazari2@gmail.com>
2022-11-15 21:31:48 +0100

    working on it

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,41 @@ import mergeImages from 'merge-images';
 import { Canvas, Image } from 'node-canvas';
 import path from 'path';
 import fs from 'fs';
+import { MersenneTwister19937, bool, real } from 'random-js';
 
 const layersPath = path.join(process.cwd(), 'layers');
 console.log(layersPath);
 
 const layers = [
-  path.join(layersPath, 'background', '1.png'),
-  path.join(layersPath, 'beard_and_mustache', '3.png'),
-  path.join(layersPath, 'body', '3.png'),
+  path.join(layersPath, 'Background', '2.png'),
+  path.join(layersPath, 'body', '1.png'),
   path.join(layersPath, 'eyes', '2.png'),
-  path.join(layersPath, 'Glasses', '1_r.png'),
-  path.join(layersPath, 'Mouths', '1.png'),
-  path.join(layersPath, 'Outfits', '3.png'),
+  path.join(layersPath, 'Glasses', '3_r.png'),
+  path.join(layersPath, 'Mouths', '2.png'),
+  path.join(layersPath, 'Outfits', '4.png'),
 ];
-async function MergeLayers(layers: any, output: any) {
-  const image = await mergeImages(layers, { Canvas: Canvas, Image: Image });
-  const base64 = image.split(',')[1];
+
+function Base64ImageSave(base64Image: string, filepath: string) {
+  const base64 = base64Image.split(',')[1];
   const imageBuffer = Buffer.from(base64, 'base64');
-  fs.writeFileSync(output, imageBuffer);
+  fs.writeFileSync(filepath, imageBuffer);
 }
 
-MergeLayers(layers, path.join(process.cwd(), 'output', '1.png'));
+async function LayersMerge(layers: string[], output: string) {
+  console.log(layers);
+
+  const image = await mergeImages(layers, { Canvas: Canvas, Image: Image });
+  Base64ImageSave(image, output);
+}
+
+// LayersMerge(layers, path.join(process.cwd(), 'output', '2.png'));
+
+function random(layersPath: string, layers: any[]) {
+  // MersenneTwister19937 is the largest randomizer
+  const randomizer = MersenneTwister19937.autoSeed();
+  console.log(randomizer);
+  for (const layer of layers) {
+    if (bool(layer.probability)(randomizer)) {
+    }
+  }
+}
